f2086a1c105328302ed930b3d1ae2b0fcc570054
Toqua Magdy <toquamagdy25@gmail.com>
2024-05-16 13:26:26 +0300

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -57,8 +57,9 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const BottomNavbar()
-      // splash1(),
+      home:
+      // const BottomNavbar()
+      const splash1(),
     );
   }
 }
