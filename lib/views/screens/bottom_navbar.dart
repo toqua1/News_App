ea4ea247f071fb169218c0db192b26f6f7e1180f
Toqua Magdy <toquamagdy25@gmail.com>
2024-05-20 13:06:53 +0300

    .

--- lib/views/screens/bottom_navbar.dart
+++ lib/views/screens/bottom_navbar.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:news_app/views/screens/bookmark_page.dart';
 import 'package:news_app/views/screens/home_page.dart';
 import 'package:news_app/views/screens/profile_page.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
